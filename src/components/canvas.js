63241b3a052f50f117070e0cc260cc7da20058ac
V1shvesh <vishvesh.ext@grofers.com>
2020-09-05 20:29:43 +0530

    Create canvas with scenes

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,43 +1,142 @@
 import initCanvasDimensions, { getCanvasWidth, getCanvasHeight } from "../utils/dimensions";
-import cake from "../shapes/cake";
-import knife from "../shapes/knife";
 import radians from "../utils/radians";
+import sceneStates, { getSceneState, setSceneState } from "../utils/SceneState";
+
+import Background from '../shapes/Background';
+import Cake from "../shapes/Cake";
+import Knife from "../shapes/Knife";
+import Candle from '../shapes/Candle'
+import CandleBlower from "../shapes/CandleBlower";
+import distance from "../utils/distance";
+import ArrowStrip from "../shapes/ArrowStrip";
+import Text from "../shapes/Text";
 
 export default () => {
   const canvas = document.querySelector('canvas');
   initCanvasDimensions(canvas);
-  let mouseX = getCanvasWidth()/2;
-  let mouseY = getCanvasHeight()/2;
 
-  window.addEventListener('mousemove', event => {
-    mouseX = event.x;
-    mouseY = event.y;
-  });
+  const background = new Background();
+  
+  const backCakeSlice = new Cake(
+    getCanvasWidth()/2,
+    getCanvasHeight()/2,
+    radians(180),
+    0,
+  );
 
-  let step = 1;
+  const acuteCakeSlice = new Cake(
+    getCanvasWidth()/2,
+    getCanvasHeight()/2,
+    0,
+    radians(45),
+  );
 
-  window.addEventListener('mouseup', () => {
-    step = 1 - step;
-  });
+  const obtuseCakeSlice = new Cake(
+    getCanvasWidth()/2,
+    getCanvasHeight()/2,
+    radians(45),
+    radians(180),
+  );
 
-  const ctx = canvas.getContext('2d');
+  const candles = [
+    new Candle(getCanvasWidth()/2, getCanvasHeight()/2 - 60),
+    new Candle(getCanvasWidth()/2 + 80, getCanvasHeight()/2 - 180),
+    new Candle(getCanvasWidth()/2 - 80, getCanvasHeight()/2 - 180),
+  ];
 
-  function background() {
-    ctx.save();
-    ctx.fillStyle = '#333333';
-    ctx.fillRect(0, 0, getCanvasWidth(), getCanvasHeight());
-    ctx.restore();
-  }
+  const fan = new CandleBlower(getCanvasWidth()/4, getCanvasHeight()/4);
+
+  const knife = new Knife(getCanvasWidth()/2 + 150);
+  const arrowStrip = new ArrowStrip(getCanvasWidth()/2 + 350, getCanvasHeight()/2);
+
+  const birthdayWish = new Text("Happy Birthday!", getCanvasWidth()/2, 100);
 
   function draw() {
-    requestAnimationFrame(draw);
-    background();
-    cake(ctx, radians(180), 0);
-    step === 0 && knife(ctx, getCanvasWidth()/2 + 150, mouseY, step);
-    cake(ctx, 0, radians(45));
-    step === 1 && knife(ctx, getCanvasWidth()/2 + 150, mouseY, step);
-    cake(ctx, radians(45), radians(180));
+    background.draw();
+
+    switch(getSceneState()) {
+      case sceneStates.INIT: {
+        backCakeSlice.draw();
+        acuteCakeSlice.draw();
+        obtuseCakeSlice.draw();
+        candles.forEach(candle => candle.draw());
+        break;
+      }
+
+      case sceneStates.CANDLE_BLOWING: {
+        backCakeSlice.draw();
+        acuteCakeSlice.draw();
+        obtuseCakeSlice.draw();
+        candles.forEach(candle => candle.draw());
+        fan.draw();
+        break;
+      }
+
+      case sceneStates.CAKE_CUTTING: {
+        backCakeSlice.draw();
+        knife.draw();
+        acuteCakeSlice.draw();
+        // step === 1 && knife(ctx, getCanvasWidth()/2 + 150, mouseY, step);
+        obtuseCakeSlice.draw();
+        arrowStrip.draw()
+        birthdayWish.draw();
+        break;
+      }
+
+      default: {
+        backCakeSlice.draw();
+        acuteCakeSlice.draw();
+        obtuseCakeSlice.draw();
+        break;
+      }
+    }
   }
 
-  draw();
+  function update() {
+
+    switch(getSceneState()) {
+      case sceneStates.CANDLE_BLOWING: {
+        candles.forEach(candle => {
+          candle.update();
+        });
+        fan.update();
+
+        candles.forEach(candle => {
+          if(distance(candle.getPosition(), fan.getPosition()) < 40) {
+            candle.extinguish();
+          }
+        });
+
+        if(candles.every(candle => candle.getState() === 'extinguished')) {
+          setSceneState(sceneStates.CAKE_CUTTING);
+        }
+        break;
+      }
+
+      case sceneStates.CAKE_CUTTING: {
+        knife.update();
+        arrowStrip.update();
+        birthdayWish.update();
+        const { y } = knife.getPosition();
+        if(knife.animate && (y >= getCanvasHeight()/2 - 90)) {
+          knife.stopAnimating()
+          arrowStrip.fillAll();
+          birthdayWish.show();
+        }
+        break;
+      }
+
+      default: {
+        break;
+      }
+    }
+  }
+
+  function loop() {
+    requestAnimationFrame(loop);
+    update();
+    draw();
+  }
+
+  loop();
 }
